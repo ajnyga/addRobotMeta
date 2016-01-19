3320679cb5f880092f4d45a1f5572ea726d081b2
ajnyga <ajnyga@gmail.com>
2016-01-19 12:55:13 +0200

    Update AddRobotMetaPlugin.inc.php

--- AddRobotMetaPlugin.inc.php
+++ AddRobotMetaPlugin.inc.php
@@ -3,8 +3,7 @@
 /**
  * @file plugins/generic/addRobotMeta/AddRobotMetaPlugin.inc.php
  *
- * Copyright (c) 2013-2015 Simon Fraser University Library
- * Copyright (c) 2000-2015 John Willinsky
+ * Copyright (c) 2015 Antti-Jussi Nygård
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @class AddRobotMetaPlugin
